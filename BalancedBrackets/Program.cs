b1978e062bbc0164f8a17007f2271d2f9fd5c757
Ronaldeived Silva <ronaldeivedsilva@MacBook.local>
2023-02-03 18:56:01 -0300

    Adding the files from local repository

---
+++ BalancedBrackets/Program.cs
@@ -0,0 +1,38 @@
+﻿
+Dictionary<char, char> bracketsPairs = new Dictionary<char, char>
+{
+        { '(', ')' },
+        { '[', ']' },
+        { '{', '}' },
+};
+
+string IsValid(string input)
+{
+    if (input.Count() == 0) return "Is Valid";
+
+    Stack<char> bracketsOpen = new Stack<char>();
+
+    foreach (char i in input)
+    {
+        if (bracketsPairs.ContainsKey(i)) bracketsOpen.Push(i);
+        else if (bracketsPairs.Values.Contains(i))
+        {
+            if (bracketsOpen.Count == 0) return "Is Invalid";
+
+            var openingBracket = bracketsOpen.Pop();
+
+            if (bracketsPairs[openingBracket] != i)
+            {
+                return "Is Invalid";
+            }
+        }
+    }
+
+    return bracketsOpen.Count == 0 ? "Is Valid" : "Is Invalid";
+}
+
+
+Console.WriteLine(IsValid("(){}[]"));
+Console.WriteLine(IsValid("[{()}](){}"));
+Console.WriteLine(IsValid("[]{()"));
+Console.WriteLine(IsValid("[{)]"));
